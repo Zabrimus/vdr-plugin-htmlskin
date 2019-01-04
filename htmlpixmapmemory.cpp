b036420999ce1862e635da1950c327ca11d0491b
Zabrimus <github@hannebauer.org>
2019-01-04 09:09:44 +0100

    initial version of htmlpixmapmemory

---
+++ htmlpixmapmemory.cpp
@@ -0,0 +1,13 @@
+/**
+ *  VDR Skin Plugin which uses CEF OSR as rendering engine
+ *
+ *  htmlpixmapmemory.cpp
+ *
+ *  (c) 2019 Robert Hannebauer
+ *
+ * This code is distributed under the terms and conditions of the
+ * GNU GENERAL PUBLIC LICENSE. See the file COPYING for details.
+ *
+ **/
+
+#include "htmlpixmapmemory.h"
---
+++ htmlpixmapmemory.h
@@ -0,0 +1,22 @@
+/**
+ *  VDR Skin Plugin which uses CEF OSR as rendering engine
+ *
+ *  htmlpixmapmemory.h
+ *
+ *  (c) 2019 Robert Hannebauer
+ *
+ * This code is distributed under the terms and conditions of the
+ * GNU GENERAL PUBLIC LICENSE. See the file COPYING for details.
+ *
+ **/
+
+#ifndef HTMLPIXMAPMEMORY_H
+#define HTMLPIXMAPMEMORY_H
+
+
+class HtmlPixmapmemory {
+
+};
+
+
+#endif // HTMLPIXMAPMEMORY_H
