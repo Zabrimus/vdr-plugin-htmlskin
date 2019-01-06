23c79e916ea50025a1eac25594941c8bf48d0111
Zabrimus <github@hannebauer.org>
2019-01-06 15:49:39 +0100

    fixed height of scrollbar

--- skin/src/classic/components/subcomponents/scrollbar.vue
+++ skin/src/classic/components/subcomponents/scrollbar.vue
@@ -1,7 +1,7 @@
 <template>
-    <div v-if="calcsb.height > 0">
-        <svg viewBox="0 0 20 600" class="sbsvg" preserveAspectRatio="none">
-            <rect id="eins" x="0" y="0"   width="20" height="600" class="scrollbar_total" />
+    <div class="scrolldiv" v-if="calcsb.height > 0">
+        <svg :viewBox="viewbox" class="sbsvg" preserveAspectRatio="none">
+            <rect id="eins" x="0" y="0"   width="20" :height="scrolldivHeight" class="scrollbar_total" />
             <rect id="zwei" x="0" :y="calcsb.y" width="20" :height="calcsb.height" class="scrollbar_sel" />
         </svg>
     </div>
@@ -16,17 +16,31 @@ export default {
       return this.$myStore.state.payload.scrollbar
     },
 
+    scrolldivHeight () {
+      return document.getElementsByClassName('scrollbar').item(0).clientHeight
+    },
+
+    viewbox () {
+      console.log('DIV HEIGHT: ' + this.scrolldivHeight)
+
+      return '0 0 20 ' + this.scrolldivHeight
+    },
+
     calcsb () {
       if (typeof this.scrollbar === 'undefined' || this.scrollbar === null) {
         return { height: 0, y: 0 }
       }
 
       if (this.scrollbar.total > 0 && this.scrollbar.total > this.scrollbar.max) {
-        var ch = 600 * this.scrollbar.max / this.scrollbar.total
-        var cy = 600 * this.scrollbar.offset / this.scrollbar.total
+        var ch = this.scrolldivHeight * this.scrollbar.max / this.scrollbar.total
+        var cy = this.scrolldivHeight * this.scrollbar.offset / this.scrollbar.total
+
+        console.log('Scrollbar: Height: ' + ch + ', y ' + cy)
 
         return { height: ch, y: cy }
       } else {
+        console.log('Scrollbar: Height: 0')
+
         return { height: 0, y: 0 }
       }
     }
@@ -36,6 +50,10 @@ export default {
 
 <style scoped lang="less">
     @import '../../assets/globalstyle.less';
+
+    .scrolldiv {
+        height: 100%;
+    }
 
     .scrollbar {
         width: 10px;
